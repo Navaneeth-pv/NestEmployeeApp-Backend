437be3240f1283070d8817051a57dcde8ebd8ed4
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-11 11:45:18 +0530

    first commit

---
+++ src/main/java/com/example/NestEmployeeAppbackend/NestEmployeeAppbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestEmployeeAppbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestEmployeeAppbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestEmployeeAppbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NestEmployeeAppbackend/NestEmployeeAppbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NestEmployeeAppbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestEmployeeAppbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
