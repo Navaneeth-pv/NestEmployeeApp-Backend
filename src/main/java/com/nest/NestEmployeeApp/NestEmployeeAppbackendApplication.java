ae13a20c7193bfad11b330aff4f10262df442622
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-11 12:19:55 +0530

    admin

--- src/main/java/com/nest/NestEmployeeApp/NestEmployeeAppbackendApplication.java
+++ src/main/java/com/nest/NestEmployeeApp/NestEmployeeAppbackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.NestEmployeeAppbackend;
+package com.nest.NestEmployeeApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/nest/NestEmployeeApp/NestEmployeeAppbackendApplicationTests.java
+++ src/test/java/com/nest/NestEmployeeApp/NestEmployeeAppbackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.NestEmployeeAppbackend;
+package com.nest.NestEmployeeApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
